60b31b9ada8cb38e0321b078604ec18c8f116de6
Alexander <abeaumier@deloitte.com>
2024-05-08 09:32:32 -0700

    API added

--- Front end/src/App.js
+++ Front end/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Components/Pages/Shop';
+import ShopCategory from './Components/Pages/ShopCategory';
+import Product from './Components/Pages/Product';
+import Cart from './Components/Pages/Cart';
+import LoginSignup from './Components/Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import embroiderymachine from './Components/Assets/embroiderymachine.jpeg';
+import PreMadeLogo from './Components/Assets/PreMadeLogo.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Shop/>}/>
+          <Route path="/customizable" element={<ShopCategory banner={embroiderymachine} category="customizable"/>}/>
+          <Route path="/pre-made" element={<ShopCategory banner={PreMadeLogo} category="pre-made"/>}/>
+          <Route path="/product" element={<Product/>}/>
+            <Route path="/productid" element={<Product/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/login" element={<LoginSignup/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
